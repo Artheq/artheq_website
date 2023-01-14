97119de6076632557f2c9883f38bfa603489f3b4
serhat111 <49848798+serhat111@users.noreply.github.com>
2023-01-13 19:21:47 -0600

    creating about

--- src/components/WhoPage/index.js
+++ src/components/WhoPage/index.js
@@ -1,11 +1,19 @@
 import React from 'react'
-import { WhoContainer } from './whoElements';
+import { WhoContainer, WhoHeading,WhoTextBox,WhoP,WhoImgContainer , WhoP1, Img} from './whoElements';
+import Icon1 from '../../images/i2.png'
 
 const WhoPage = () => {
   return (
     <>
     <WhoContainer>
-        
+        <WhoTextBox>
+          <WhoHeading>Our Purpose & Mission</WhoHeading>
+          <WhoP>Our purpose is to unleash the power of human potential by making communication more effective.</WhoP>
+          <WhoP1>Our mission is to enable people and organizations to maximize their potential using the power of self-discovery,positive communication, personal branding, and coaching.</WhoP1>
+        </WhoTextBox>
+        <WhoImgContainer>
+            <Img src={Icon1}></Img>
+        </WhoImgContainer>
     </WhoContainer>
     </>
   )
--- src/components/WhoPage/whoElements.js
+++ src/components/WhoPage/whoElements.js
@@ -1,10 +1,46 @@
 import styled from "styled-components";
 
 export const WhoContainer = styled.div`
-    background:black;
-    height:550px;
+    background:#f9f9f9;
+    height:500px;
+    display:flex;
     
     @media screen and (max-width: 768px) {
     padding: 100px 0;
+    height:600px;
     }
+`
+export const WhoTextBox = styled.div`
+    padding:50px;
+    display:grid;
+    align-content:center;
+    justify-content:center;
+    flex:1;
+`
+
+export const WhoHeading = styled.h1`
+    font-size:3em;
+`
+
+export const WhoP = styled.p`
+    font-size:1.2em;
+    padding-top:10px;
+`
+export const WhoP1 = styled.p`
+    font-size:1.2em;
+    padding-top:25px;
+`
+
+export const WhoImgContainer = styled.div`
+    flex:1;
+    display:flex;
+    align-content:center;
+    justify-content:center;
+    align-items:center;
+`
+
+export const Img = styled.img`
+    border-radius:50px;
+    width:450px;
+    height:450px;
 `
